e40817d85386d888d893b01b520f4ef858343bbf
Thanh Huy <huyvct@hiworld.com.vn>
2023-01-30 20:49:54 +0700

    first commit

---
+++ src/main/java/com/project/Bsmart/BsmartApplication.java
@@ -0,0 +1,13 @@
+package com.project.Bsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Bsmart/BsmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Bsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
