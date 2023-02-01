ddf702dfaddc48133db8a4fd5b2e84d83dd4b57d
Thanh Huy <huyvct@hiworld.com.vn>
2023-02-01 11:04:30 +0700

    test create entity

---
+++ src/main/java/com/project/Bsmart/entity/Account.java
@@ -0,0 +1,19 @@
+package com.project.Bsmart.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
