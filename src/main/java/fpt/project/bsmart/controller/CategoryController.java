63114963ab4d24fbf599549efea037bf6a9f1d83
Lê Thiên Ân <80709126+ThienAnn-SE@users.noreply.github.com>
2023-03-05 00:45:07 +0700

    chore: rename ICategory vairable

--- src/main/java/fpt/project/bsmart/controller/CategoryController.java
+++ src/main/java/fpt/project/bsmart/controller/CategoryController.java
@@ -13,33 +13,33 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/categories")
 public class CategoryController {
-    private final ICategoryService categoryService;
-    public CategoryController (ICategoryService categoryService){
-        this.categoryService = categoryService;
+    private final ICategoryService iCategoryService;
+    public CategoryController (ICategoryService iCategoryService){
+        this.iCategoryService = iCategoryService;
     }
 
     @GetMapping
     public ResponseEntity<ApiResponse<List<CategoryDto>>> getAllCategories(){
-        return ResponseEntity.ok(ApiResponse.success(categoryService.getAllCategories()));
+        return ResponseEntity.ok(ApiResponse.success(iCategoryService.getAllCategories()));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponse<CategoryDto>> getCategory(@PathVariable Long id){
-        return ResponseEntity.ok(ApiResponse.success(categoryService.getCategory(id)));
+        return ResponseEntity.ok(ApiResponse.success(iCategoryService.getCategory(id)));
     }
 
     @PostMapping
     public ResponseEntity<ApiResponse<Long>> createCategory(@Valid @RequestBody CategoryRequest categoryRequest){
-        return ResponseEntity.ok(ApiResponse.success(categoryService.createCategory(categoryRequest)));
+        return ResponseEntity.ok(ApiResponse.success(iCategoryService.createCategory(categoryRequest)));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<ApiResponse<Long>> updateCategory(@PathVariable Long id, @Valid @RequestBody CategoryRequest categoryRequest){
-        return ResponseEntity.ok(ApiResponse.success(categoryService.updateCategory(id, categoryRequest)));
+        return ResponseEntity.ok(ApiResponse.success(iCategoryService.updateCategory(id, categoryRequest)));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse<Long>> deleteCategory(@PathVariable Long id){
-        return ResponseEntity.ok(ApiResponse.success(categoryService.deleteCategory(id)));
+        return ResponseEntity.ok(ApiResponse.success(iCategoryService.deleteCategory(id)));
     }
 }
