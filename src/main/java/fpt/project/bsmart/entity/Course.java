cfce982691609159de68dec12764479cb046350f
dangnlh <dangnlh@hiworld.com.vn>
2023-03-05 09:36:32 +0700

    fix entity course and add image

--- src/main/java/fpt/project/bsmart/entity/Course.java
+++ src/main/java/fpt/project/bsmart/entity/Course.java
@@ -19,25 +19,39 @@ public class Course {
     @Column(name = "code")
     private String code;
 
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "status")
+    private boolean status;
+
+
     @Column(name = "type")
     @Enumerated(EnumType.STRING)
     private ETypeLearn type;
-    @Column(name = "description")
-    private String description;
-    @Column(name = "reference_discount")
-    private Double referenceDiscount = 0.0;
+
     @Column(name = "level")
     @Enumerated(EnumType.STRING)
     private ECourseLevel level;
+
+    @Column(name = "reference_discount")
+    private Double referenceDiscount = 0.0;
+
     @ManyToOne
     @JoinColumn(name = "subject_id")
     private Subject subject;
 
-    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Section> sections = new ArrayList<>();
     @ManyToOne
     @JoinColumn(name = "mentor_id")
     private User mentor;
+
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "image_id", referencedColumnName = "id")
+    private Image image;
+
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Section> sections = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -63,14 +77,6 @@ public class Course {
         this.code = code;
     }
 
-    public ETypeLearn getType() {
-        return type;
-    }
-
-    public void setType(ETypeLearn type) {
-        this.type = type;
-    }
-
     public String getDescription() {
         return description;
     }
@@ -79,12 +85,20 @@ public class Course {
         this.description = description;
     }
 
-    public Double getReferenceDiscount() {
-        return referenceDiscount;
+    public boolean isStatus() {
+        return status;
     }
 
-    public void setReferenceDiscount(Double referenceDiscount) {
-        this.referenceDiscount = referenceDiscount;
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public ETypeLearn getType() {
+        return type;
+    }
+
+    public void setType(ETypeLearn type) {
+        this.type = type;
     }
 
     public ECourseLevel getLevel() {
@@ -95,6 +109,14 @@ public class Course {
         this.level = level;
     }
 
+    public Double getReferenceDiscount() {
+        return referenceDiscount;
+    }
+
+    public void setReferenceDiscount(Double referenceDiscount) {
+        this.referenceDiscount = referenceDiscount;
+    }
+
     public Subject getSubject() {
         return subject;
     }
@@ -103,19 +125,27 @@ public class Course {
         this.subject = subject;
     }
 
-    public List<Section> getSections() {
-        return sections;
-    }
-
-    public void setSections(List<Section> sections) {
-        this.sections = sections;
-    }
-
     public User getMentor() {
         return mentor;
     }
 
     public void setMentor(User mentor) {
         this.mentor = mentor;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public List<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(List<Section> sections) {
+        this.sections = sections;
     }
 }
