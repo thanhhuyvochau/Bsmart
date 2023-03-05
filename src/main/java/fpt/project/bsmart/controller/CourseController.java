54ea3df450b1f049d21c0cac27355e4353f2bb64
dangnlh <dangnlh@hiworld.com.vn>
2023-03-05 08:59:19 +0700

    api mentor create course

--- src/main/java/fpt/project/bsmart/controller/CourseController.java
+++ src/main/java/fpt/project/bsmart/controller/CourseController.java
@@ -1,9 +1,30 @@
 package fpt.project.bsmart.controller;
 
+import fpt.project.bsmart.entity.common.ApiResponse;
+import fpt.project.bsmart.entity.request.subject.SubjectRequest;
+import fpt.project.bsmart.service.ICourseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
+
+
+    private final ICourseService iCourseService ;
+
+    public CourseController(ICourseService iCourseService) {
+        this.iCourseService = iCourseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Long>> mentorCreateCourse(@Valid @RequestBody SubjectRequest subjectRequest){
+        return ResponseEntity.ok(ApiResponse.success(iCourseService.mentorCreateCourse(subjectRequest)));
+    }
+
 }
