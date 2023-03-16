7d486be276829bbfa1496de039e775375df107ac
Thanh Huy <huyvct@hiworld.com.vn>
2023-03-16 21:40:04 +0700

    feat: pay course

--- src/main/java/fpt/project/bsmart/service/Impl/TransactionService.java
+++ src/main/java/fpt/project/bsmart/service/Impl/TransactionService.java
@@ -1,18 +1,13 @@
 package fpt.project.bsmart.service.Impl;
 
-import fpt.project.bsmart.entity.Bank;
-import fpt.project.bsmart.entity.Transaction;
-import fpt.project.bsmart.entity.User;
-import fpt.project.bsmart.entity.Wallet;
+import fpt.project.bsmart.entity.*;
 import fpt.project.bsmart.entity.common.ApiException;
 import fpt.project.bsmart.entity.common.ApiPage;
+import fpt.project.bsmart.entity.constant.EOrderStatus;
 import fpt.project.bsmart.entity.constant.ETransactionType;
 import fpt.project.bsmart.entity.dto.TransactionDto;
 import fpt.project.bsmart.entity.request.WithdrawRequest;
-import fpt.project.bsmart.repository.BankRepository;
-import fpt.project.bsmart.repository.TransactionRepository;
-import fpt.project.bsmart.repository.UserRepository;
-import fpt.project.bsmart.repository.WalletRepository;
+import fpt.project.bsmart.repository.*;
 import fpt.project.bsmart.service.ITransactionService;
 import fpt.project.bsmart.util.ConvertUtil;
 import fpt.project.bsmart.util.MessageUtil;
@@ -26,6 +21,8 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.List;
 
+import static fpt.project.bsmart.util.Constants.ErrorMessage.COURSE_NOT_FOUND_BY_ID;
+
 @Service
 public class TransactionService implements ITransactionService {
 
@@ -36,12 +33,18 @@ public class TransactionService implements ITransactionService {
 
     private final BankRepository bankRepository;
 
-    public TransactionService(WalletRepository walletRepository, TransactionRepository transactionRepository, UserRepository userRepository, MessageUtil messageUtil, BankRepository bankRepository) {
+    private final CourseRepository courseRepository;
+
+    private final SubCourseRepository subCourseRepository;
+
+    public TransactionService(WalletRepository walletRepository, TransactionRepository transactionRepository, UserRepository userRepository, MessageUtil messageUtil, BankRepository bankRepository, CourseRepository courseRepository, SubCourseRepository subCourseRepository) {
         this.walletRepository = walletRepository;
         this.transactionRepository = transactionRepository;
         this.userRepository = userRepository;
         this.messageUtil = messageUtil;
         this.bankRepository = bankRepository;
+        this.courseRepository = courseRepository;
+        this.subCourseRepository = subCourseRepository;
     }
 
     @Override
@@ -67,7 +70,7 @@ public class TransactionService implements ITransactionService {
         Wallet wallet = SecurityUtil.getCurrentUserWallet();
         // Nạp từ 10 nghìn trở lên
         if (amount.compareTo(BigDecimal.valueOf(10000)) <= 0) {
-            return false;
+           throw ApiException.create(HttpStatus.CONFLICT).withMessage("Bạn phải nạp từ 10.000 VNĐ trờ lên!");
         }
         Transaction transaction = Transaction.build(amount, null, null, null, wallet, ETransactionType.DEPOSIT);
         wallet.setBalance(wallet.getBalance().add(amount));
@@ -85,6 +88,38 @@ public class TransactionService implements ITransactionService {
         Bank bank = bankRepository.findById(request.getBankId()).orElseThrow(() -> ApiException.create(HttpStatus.NOT_FOUND).withMessage("Không tìm thấy ngân hàng hỗ trợ xin thử lại!"));
         Transaction transaction = Transaction.build(amount, request.getBankAccount(), request.getBankAccountOwner(), bank, wallet, ETransactionType.WITHDRAW);
         wallet.setBalance(wallet.getBalance().subtract(amount));
+        transactionRepository.save(transaction);
+        return true;
+    }
+
+    @Override
+    public Boolean payCourse(Long subCourseId) {
+        SubCourse subCourse = subCourseRepository.findById(subCourseId)
+                .orElseThrow(() -> ApiException.create(HttpStatus.NOT_FOUND).withMessage(messageUtil.getLocalMessage(COURSE_NOT_FOUND_BY_ID) + subCourseId));
+        BigDecimal price = subCourse.getPrice();
+        Wallet wallet = SecurityUtil.getCurrentUserWallet();
+        BigDecimal presentBalance = wallet.getBalance();
+        if (presentBalance.compareTo(price) < 0) {
+            throw ApiException.create(HttpStatus.NOT_FOUND).withMessage("Số dư của bạn không đủ để thanh toán khóc học này, vui lòng nạp thêm!");
+        }
+        // TODO: Tạm thời chưa xử lý khuyến mãi, sẽ bổ sung xử lý KM sau
+        OrderDetail orderDetail = new OrderDetail();
+        orderDetail.setSubCourse(subCourse);
+        orderDetail.setFinalPrice(price);
+        orderDetail.setOriginalPrice(price);
+
+        Order order = new Order();
+        order.setStatus(EOrderStatus.SUCCESS);
+        order.setTotalPrice(price);
+        order.getOrderDetails().add(orderDetail);
+
+        Transaction transaction = new Transaction();
+        transaction.setOrder(order);
+        transaction.setWallet(wallet);
+        transaction.setType(ETransactionType.PAY);
+        transaction.setBeforeBalance(presentBalance);
+        transaction.setAfterBalance(presentBalance.subtract(price));
+
         transactionRepository.save(transaction);
         return true;
     }
