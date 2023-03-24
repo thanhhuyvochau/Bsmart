3cf6a249ca53e3d027cfc11b22515879f7f65f47
dangnlh <dangnlh@hiworld.com.vn>
2023-03-24 20:54:53 +0700

    chro: add enum image

--- src/main/java/fpt/project/bsmart/entity/constant/EImageType.java
+++ src/main/java/fpt/project/bsmart/entity/constant/EImageType.java
@@ -5,7 +5,9 @@ public enum EImageType {
     COURSE("Ảnh khoa hoc"),
     AVATAR("Ảnh đại diện"),
     FRONTCI("Mặt trước Căn cước công dân") ,
-    BACKCI("Mặt sau ăn cước công dân");
+    BACKCI("Mặt sau ăn cước công dân"),
+
+    DEGREE("bằng cấp") ;
     private final String name;
 
     EImageType(String name) {
