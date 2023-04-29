6b1b835c3b8515a5db633d0c04523d47ab3aecb5
Thanh Huy <huyvct@hiworld.com.vn>
2023-04-29 10:48:55 +0700

    refactor: add student class when create class from sub course

--- src/main/java/fpt/project/bsmart/repository/OrderDetailRepository.java
+++ src/main/java/fpt/project/bsmart/repository/OrderDetailRepository.java
@@ -1,14 +1,14 @@
 package fpt.project.bsmart.repository;
 
 import fpt.project.bsmart.entity.OrderDetail;
-import fpt.project.bsmart.entity.ReferralCode;
+import fpt.project.bsmart.entity.SubCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 
 @Repository
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-
+    List<OrderDetail> findAllBySubCourse(SubCourse subCourse);
 }
