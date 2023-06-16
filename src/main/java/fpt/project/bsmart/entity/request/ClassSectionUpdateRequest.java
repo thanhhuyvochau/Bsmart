018eaea8d92348d482b28a7f24cb78ae028f90c0
Thanh Huy <huyvct@hiworld.com.vn>
2023-06-16 22:09:50 +0700

    feat: CRUD class section

---
+++ src/main/java/fpt/project/bsmart/entity/request/ClassSectionUpdateRequest.java
@@ -0,0 +1,15 @@
+package fpt.project.bsmart.entity.request;
+
+public class ClassSectionUpdateRequest {
+    private String name;
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- src/main/java/fpt/project/bsmart/service/IClassService.java
+++ src/main/java/fpt/project/bsmart/service/IClassService.java
@@ -2,20 +2,33 @@ package fpt.project.bsmart.service;
 
 import fpt.project.bsmart.entity.common.ApiPage;
 import fpt.project.bsmart.entity.dto.ClassProgressTimeDto;
+import fpt.project.bsmart.entity.dto.ClassSectionDto;
 import fpt.project.bsmart.entity.request.ClassFeedbackRequest;
 import fpt.project.bsmart.entity.request.ClassFilterRequest;
+import fpt.project.bsmart.entity.request.ClassSectionCreateRequest;
+import fpt.project.bsmart.entity.request.ClassSectionUpdateRequest;
 import fpt.project.bsmart.entity.request.category.CreateClassRequest;
 import fpt.project.bsmart.entity.response.ClassResponse;
 import fpt.project.bsmart.entity.response.SimpleClassResponse;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface IClassService {
 
     Boolean createClass(CreateClassRequest request);
+
     ApiPage<SimpleClassResponse> getClassFeedbacks(ClassFeedbackRequest classFeedbackRequest, Pageable pageable);
+
     ClassProgressTimeDto getClassProgression(Long clazzId);
+
     ClassResponse getDetailClass(Long id);
+
     ApiPage<SimpleClassResponse> getUserClasses(ClassFilterRequest request, Pageable pageable);
+
+    ClassSectionDto createClassSection(ClassSectionCreateRequest request, Long classId);
+
+    ClassSectionDto getClassSection(Long classSectionId,Long classId);
+
+    ClassSectionDto updateClassSection(Long classId, Long classSectionId, ClassSectionUpdateRequest request);
+
+    Boolean deleteClassSection(Long classSectionId,Long classId);
 }
