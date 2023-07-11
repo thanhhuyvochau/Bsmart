fe1ab1c629cdf4ed01dc62e1b8df3165d65d46ad
Lê Thiên Ân <80709126+ThienAnn-SE@users.noreply.github.com>
2023-07-11 19:31:14 +0700

    Update ActivityController.java

--- src/main/java/fpt/project/bsmart/controller/ActivityController.java
+++ src/main/java/fpt/project/bsmart/controller/ActivityController.java
@@ -5,6 +5,8 @@ import fpt.project.bsmart.entity.dto.ActivityDto;
 import fpt.project.bsmart.entity.request.AddQuizRequest;
 import fpt.project.bsmart.entity.request.AssignmentRequest;
 import fpt.project.bsmart.entity.request.SubmitAssignmentRequest;
+import fpt.project.bsmart.entity.request.activity.MentorCreateAnnouncementForClass;
+import fpt.project.bsmart.entity.request.activity.MentorCreateResourceRequest;
 import fpt.project.bsmart.entity.request.activity.MentorCreateSectionForCourse;
 import fpt.project.bsmart.entity.response.Avtivity.MentorDeleteSectionForCourse;
 import fpt.project.bsmart.entity.response.Avtivity.MentorGetSectionForCourse;
