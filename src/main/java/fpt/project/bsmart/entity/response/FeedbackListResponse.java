0e49180f32b9fad50bf692713c02120462b1decb
Lê Thiên Ân <80709126+ThienAnn-SE@users.noreply.github.com>
2023-07-25 21:06:25 +0700

    refactor: remove feedback feature

--- src/main/java/fpt/project/bsmart/entity/response/FeedbackListResponse.java
+++
@@ -1,9 +0,0 @@
-package fpt.project.bsmart.entity.response;
-
-import fpt.project.bsmart.entity.constant.EFeedbackType;
-
-public class FeedbackListResponse {
-    private long feedbackAnswerId;
-    private EFeedbackType feedbackType;
-    private long totalScore;
-}
