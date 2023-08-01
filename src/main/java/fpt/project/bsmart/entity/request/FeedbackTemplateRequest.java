6dfa65a56653a3e90e994c221d0f8e3da56d702d
Lê Thiên Ân <80709126+ThienAnn-SE@users.noreply.github.com>
2023-08-01 23:56:38 +0700

    refactoring: feedback
    -refactoring feedback request object
    -change update feedback URL

--- src/main/java/fpt/project/bsmart/entity/request/FeedbackTemplateRequest.java
+++ src/main/java/fpt/project/bsmart/entity/request/FeedbackTemplateRequest.java
@@ -2,9 +2,22 @@ package fpt.project.bsmart.entity.request;
 
 import fpt.project.bsmart.entity.constant.EFeedbackType;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FeedbackTemplateRequest {
-    private EFeedbackType type;
     private String name;
+    private EFeedbackType type;
+    private List<FeedbackQuestionRequest> questions = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public EFeedbackType getType() {
         return type;
     }
@@ -13,11 +26,44 @@ public class FeedbackTemplateRequest {
         this.type = type;
     }
 
-    public String getName() {
-        return name;
+    public List<FeedbackQuestionRequest> getQuestions() {
+        return questions;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setQuestions(List<FeedbackQuestionRequest> questions) {
+        this.questions = questions;
+    }
+
+    public static class FeedbackQuestionRequest {
+        private String question;
+        private List<FeedbackAnswerRequest> answers = new ArrayList<>();
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public void setQuestion(String question) {
+            this.question = question;
+        }
+
+        public List<FeedbackAnswerRequest> getAnswers() {
+            return answers;
+        }
+
+        public void setAnswers(List<FeedbackAnswerRequest> answers) {
+            this.answers = answers;
+        }
+    }
+
+    public static class FeedbackAnswerRequest {
+        private String answer;
+
+        public String getAnswer() {
+            return answer;
+        }
+
+        public void setAnswer(String answer) {
+            this.answer = answer;
+        }
     }
 }
