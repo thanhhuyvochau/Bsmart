260c70d94deb963160ada02f48f6c66231bd31ed
Thanh Huy <huyvct@hiworld.com.vn>
2023-08-01 21:03:40 +0700

    feat: set handshaker

--- src/main/java/fpt/project/bsmart/config/socket/ClientHandshakeHandler.java
+++ src/main/java/fpt/project/bsmart/config/socket/ClientHandshakeHandler.java
@@ -18,8 +18,8 @@ public class ClientHandshakeHandler extends DefaultHandshakeHandler {
     @Override
     protected Principal determineUser(ServerHttpRequest req, WebSocketHandler weHandler, Map<String, Object> attributes) {
         final String randId = UUID.randomUUID().toString();
-        logger.debug("{}",attributes.get("name"));
-        logger.debug("User opened client unique ID {}, ipAddress {}",randId,req.getRemoteAddress());
+        logger.debug("{}", attributes.get("name"));
+        logger.debug("User opened client unique ID {}, ipAddress {}", randId, req.getRemoteAddress());
         return new UserPrincipal(randId);
     }
 
--- src/main/java/fpt/project/bsmart/config/socket/WebSocketConfig.java
+++ src/main/java/fpt/project/bsmart/config/socket/WebSocketConfig.java
@@ -1,11 +1,8 @@
 package fpt.project.bsmart.config.socket;
 
-import fpt.project.bsmart.service.Impl.UserHandshakeHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
@@ -16,9 +13,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
-                .addEndpoint("/websocket").setAllowedOriginPatterns("*") ;
+                .addEndpoint("/websocket").setAllowedOriginPatterns("*");
         registry
                 .addEndpoint("/websocket").setAllowedOriginPatterns("*")
+                .setHandshakeHandler(new ClientHandshakeHandler())
                 .withSockJS();
     }
 
